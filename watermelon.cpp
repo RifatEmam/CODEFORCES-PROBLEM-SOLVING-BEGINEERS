00e3913e72a47b56ac6bc3166a84b8e2b0d25b59
M.D Rifat Emam <153221770+RifatEmam@users.noreply.github.com>
2025-09-12 02:06:50 +0600

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+int w,i;
+cin>>w;
+if(1<=w && w<=100){
+
+            if(w%2==0 && w>2){
+                cout<<"YES";
+
+        }else{
+        cout<<"NO";
+        }
+        }
+
+
+
+
+return 0;
+}
