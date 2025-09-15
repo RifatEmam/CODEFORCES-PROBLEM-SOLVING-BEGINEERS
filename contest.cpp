77e24556e6969373001f3e1946dcd01bdf359bcb
M.D Rifat Emam <153221770+RifatEmam@users.noreply.github.com>
2025-09-16 02:19:06 +0600

    Add files via upload

---
+++ contest.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arra(n);
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arra[i];
+            sum += arra[i];
+        }
+
+        if(sum % 3 == 0)
+            cout << 1 << " " << 2 << endl;
+        else
+            cout << 0 << " " << 0 << endl;
+    }
+    return 0;
+}
